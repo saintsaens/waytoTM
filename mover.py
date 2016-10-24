64c186792e6d4f4a5cba95d874e085ced7f544dc
saintsaens <frobert@gpartner.eu>
2016-10-24 16:26:51 +0200

    Add logging for verbose.

--- mover.py
+++ mover.py
@@ -3,24 +3,34 @@
 
 import utils
 import personal_constants
+import argparse
+import logging
 
 MP3_320 = "[320]"
 MP3_V0 = "[V0]"
 MP3_V2 = "[V2]"
 MP3_FORMATS = [MP3_320, MP3_V0, MP3_V2]
 
+if __name__ == '__main__':
+    levels = ['info', 'debug', 'error', 'warning', 'critical']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--level', dest='level', default='info',
+                        choices=['info', 'debug', 'error', 'warning', 'critical'])
+    args = parser.parse_args()
+    logging.basicConfig(level=getattr(logging, args.level.upper()))
+
 # Get all subfolders in FLAC_FOLDER (level 0).
 flac_dirs_lvl_0 = utils.get_direct_subdirs(personal_constants.FLAC_FOLDER)
 
 for x in flac_dirs_lvl_0:
-    # print "Getting contents of " + x + "..."
-    # print "Checking if there is another level..."
+    logging.debug("Getting contents of " + x + "...")
+    logging.debug("Checking if there is another level...")
     if utils.get_direct_subdirs(x):
-        # print "Yes"
+        logging.debug("Yes")
         # There is another level, so dance once again: get all subdirs in the current subdir of FLAC_FOLDER (level 1).
         flac_dirs_lvl_1 = utils.get_direct_subdirs(x)
         for y in flac_dirs_lvl_1:
-            # print "Getting contents of " + y + "..."
+            logging.debug("Getting contents of " + y + "...")
             # Check if there are at least 2 directories with 320 at lvl 2, and merge them.
             if utils.check_level(y, MP3_320) == "NOK":
                 merged_dir = utils.merge_folders(y, MP3_320)
@@ -32,7 +42,6 @@ for x in flac_dirs_lvl_0:
         if utils.check_level(x, MP3_320) == "NOK":
             utils.merge_folders(x, MP3_320)
 
-# print "Checking the number of files in new folder matches number of files in FLAC folder."
-
-# print "Deleting old folders."
+logging.debug("Checking the number of files in new folder matches number of files in FLAC folder.")
+logging.debug("Deleting old folders.")
 
