071f426dde844704c31a862b57ce8587bf0ebcd6
Flavien <frobert@gpartner.eu>
2016-10-24 10:11:54 +0700

    Comment verbose. Focus on mergeables, nothing for the clean ones.

--- mover.py
+++ mover.py
@@ -12,31 +12,24 @@ MP3_V2 = "[V2]"
 # Get all subfolders in FLAC_FOLDER (level 0).
 flac_dirs_lvl_0 = utils.get_direct_subdirs(FLAC_FOLDER)
 for x in flac_dirs_lvl_0:
-    print "Getting contents of " + x + "..."
+    # print "Getting contents of " + x + "..."
     # print "Checking if there is another level..."
     if utils.get_direct_subdirs(x):
         # print "Yes"
         # There is another level, so dance once again.
         flac_dirs_lvl_1 = utils.get_direct_subdirs(x)
         for y in flac_dirs_lvl_1:
-            print "Getting contents of " + y + "..."
+            # print "Getting contents of " + y + "..."
             # Check if there are at least 2 directories with 320, and merge them.
             if utils.check_level(y, MP3_320) == "NOK":
                 merged_dir = utils.merge_folders(y, MP3_320)
-            # Copy cover art.
-            if utils.check_image(y) == 1:
-                print "Recovering image..."
-                utils.copy_images(y, merged_dir)
-            else:
-                print "No image there."
+                # Copy cover art.
+                if utils.check_image(y) == 1:
+                    utils.copy_images(y, merged_dir)
     else:
         # Check if there are at least 2 directories with 320, and merge them.
         if utils.check_level(x, MP3_320) == "NOK":
             utils.merge_folders(x, MP3_320)
-        if utils.check_image(y) == 1:
-            print "Recovering image..."
-        else:
-            print "No image there."
 
 # print "Is there a cover art in the FLAC folder?"
 
